6aedc61b2616203743fb8146326c5f150f2019c1
lizconlan <lizconlan@gmail.com>
2014-03-16 14:56:44 +0000

    mappings for lords_hansard

--- lib/mappings.rb
+++ lib/mappings.rb
@@ -1,6 +1,6 @@
 class Mappings
   def self.index_names
-    ["commons_hansard"]
+    ["commons_hansard", "lords_hansard"]
   end
   
   def self.all_mappings
@@ -8,6 +8,30 @@ class Mappings
       commons_hansard:{
         contents: {
           section: {
+            date_formats: ["date", "basic_date"],
+            properties: {
+              title: { type: "string", index: "analyzed" },
+              text:  { type: "string", index: "analyzed" },
+              date:  { type: "date", index: "analyzed" },
+              hansard_component: { type: "string", index: "not_analyzed" },
+              hansard_ref: { type: "string", index: "analyzed" },
+              bill_title: { type: "string", index: "analyzed"},
+              extended_bill_title: { type: "string", index: "analyzed"},
+              members: { type: "string", index: "not_analyzed" },
+              number: { type: "string", index: "analyzed" },
+              asked_by: { type: "string", index: "analyzed" },
+              subject: { type: "string", index: "analyzed" },
+              department: { type: "string", index: "analyzed" },
+              chair: { type: "string", index: "analyzed" },
+              url: { type: "string", index: "not_analyzed" }
+            },
+          }
+        }
+      },
+      lords_hansard:{
+        contents: {
+          section: {
+            date_formats: ["date", "basic_date"],
             properties: {
               title: { type: "string", index: "analyzed" },
               text:  { type: "string", index: "analyzed" },
